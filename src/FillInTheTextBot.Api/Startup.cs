dbf12fd7d9c41d2b87a1f7b69083b45ec7d05885
Stepan Grankin <sgrankin@inbox.ru>
2020-07-12 17:50:03 +0300

    metrics middleware only at prod

--- src/FillInTheTextBot.Api/Startup.cs
+++ src/FillInTheTextBot.Api/Startup.cs
@@ -37,7 +37,10 @@ namespace FillInTheTextBot.Api
         // ReSharper disable once UnusedMember.Global
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, AppConfiguration configuration)
         {
-            app.UseMiddleware<MetricsMiddleware>();
+            if (!env.IsDevelopment())
+            {
+                app.UseMiddleware<MetricsMiddleware>();
+            }
 
             if (configuration.HttpLog.Enabled)
             {
