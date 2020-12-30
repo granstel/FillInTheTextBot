924ccd0bd7698b23a8b72cd02d871138704084b9
Stepan Grankin <sgrankin@inbox.ru>
2020-12-30 13:44:19 +0300

    fix error answer

--- src/FillInTheTextBot.Messengers.Yandex/YandexService.cs
+++ src/FillInTheTextBot.Messengers.Yandex/YandexService.cs
@@ -17,7 +17,7 @@ namespace FillInTheTextBot.Messengers.Yandex
 
         private const string oldUSerStateKey = "isOldUser";
 
-        private const string errorAnswer = "Прости, у меня какие-то проблемы... Давай попробуем ещё раз. Если повторится, найди в ВК паблик \"Занимательные истории Алисы из Яндекса\" и напиши в личку";
+        private const string errorAnswer = "Прости, у меня какие-то проблемы... Давай попробуем ещё раз. Если повторится, найди в ВК паблик \"Занимательные истории Алисы из Яндекса\" и напиши об этом в личку";
 
         private readonly IMapper _mapper;
 
