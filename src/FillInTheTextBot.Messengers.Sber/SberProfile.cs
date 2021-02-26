d9f3b5788466c8eeb2e2cd8b44ee79fba3143a9b
Stepan Grankin <sgrankin@inbox.ru>
2021-02-26 19:58:09 +0300

    fill sber profile with mvp

--- src/FillInTheTextBot.Messengers.Sber/SberProfile.cs
+++ src/FillInTheTextBot.Messengers.Sber/SberProfile.cs
@@ -13,12 +13,12 @@ namespace FillInTheTextBot.Messengers.Sber
             CreateMap<Request, Models.Request>()
                 .ForMember(d => d.ChatHash, m => m.ResolveUsing(s => s?.Payload?.AppInfo?.ProjectId.ToString()))
                 .ForMember(d => d.UserHash, m => m.ResolveUsing(s => s?.Uuid?.Sub ?? s?.Uuid?.UserId))
-                .ForMember(d => d.Text, m => m.Ignore())
-                .ForMember(d => d.SessionId, m => m.Ignore())
-                .ForMember(d => d.NewSession, m => m.Ignore())
+                .ForMember(d => d.Text, m => m.ResolveUsing(s => s?.Payload?.Message?.AsrNormalizedMessage))
+                .ForMember(d => d.SessionId, m => m.ResolveUsing(s => s?.SessionId))
+                .ForMember(d => d.NewSession, m => m.ResolveUsing(s => s?.Payload?.NewSession))
                 .ForMember(d => d.Language, m => m.Ignore())
-                .ForMember(d => d.HasScreen, m => m.Ignore())
-                .ForMember(d => d.ClientId, m => m.Ignore())
+                .ForMember(d => d.HasScreen, m => m.ResolveUsing(s => s?.Payload?.Device?.Capabilities?.Screen?.Available ?? false))
+                .ForMember(d => d.ClientId, m => m.ResolveUsing(s => s?.Payload?.Device?.Surface))
                 .ForMember(d => d.Source, m => m.UseValue(Models.Source.Sber))
                 .ForMember(d => d.RequiredContext, m => m.Ignore())
                 .ForMember(d => d.ClearContexts, m => m.Ignore())
@@ -26,11 +26,28 @@ namespace FillInTheTextBot.Messengers.Sber
                 .ForMember(d => d.NextTextIndex, m => m.Ignore())
                 .ForMember(d => d.ScopeKey, m => m.Ignore());
 
-            CreateMap<Models.Response, Response>();
+            CreateMap<Models.Response, Response>()
+                .ForMember(d => d.Payload, m => m.MapFrom(s => s))
+                ;
 
-            CreateMap<Request, Response>();
+            CreateMap<Models.Response, ResponsePayload>()
+                .ForMember(d => d.PronounceText, m => m.MapFrom(s => s.Text))
+                .ForMember(d => d.AutoListening, m => m.MapFrom(s => !s.Finished))
+                .ForMember(d => d.Finished, m => m.MapFrom(s => s.Finished))
+                ;
 
-            CreateMap<RequestPayload, ResponsePayload>();
+            CreateMap<Request, Response>()
+                .ForMember(d => d.MessageName, m => m.UseValue("ANSWER_TO_USER"))
+                .ForMember(d => d.SessionId, m => m.MapFrom(s => s.SessionId))
+                .ForMember(d => d.MessageId, m => m.MapFrom(s => s.MessageId))
+                .ForMember(d => d.Uuid, m => m.MapFrom(s => s.Uuid))
+                .ForMember(d => d.Payload, m => m.MapFrom(s => s.Payload))
+                ;
+
+            CreateMap<RequestPayload, ResponsePayload>()
+                .ForMember(d => d.Device, m => m.MapFrom(s => s.Device))
+                .ForMember(d => d.ProjectName, m => m.MapFrom(s => s.ProjectName))
+                ;
         }
     }
 }
