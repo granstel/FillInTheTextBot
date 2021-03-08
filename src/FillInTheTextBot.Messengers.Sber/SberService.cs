d681e8beed9fa1d2e16ff68ac2db3174f730d265
Stepan Grankin <sgrankin@inbox.ru>
2021-03-08 15:15:07 +0300

    save and use user state for sber

--- src/FillInTheTextBot.Messengers.Sber/SberService.cs
+++ src/FillInTheTextBot.Messengers.Sber/SberService.cs
@@ -1,8 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
+using FillInTheTextBot.Models;
 using FillInTheTextBot.Services;
+using GranSteL.Helpers.Redis;
 using NLog;
-using Sber.SmartApp.Models;
+using Request = Sber.SmartApp.Models.Request;
+using Response = Sber.SmartApp.Models.Response;
 
 namespace FillInTheTextBot.Messengers.Sber
 {
@@ -11,15 +16,36 @@ namespace FillInTheTextBot.Messengers.Sber
         private const string ErrorAnswer = "Прости, у меня какие-то проблемы... Давай попробуем ещё раз. Если повторится, найди в ВК паблик \"Занимательные истории Алисы из Яндекса\" и напиши об этом в личку";
 
         private readonly IMapper _mapper;
+        private readonly IRedisCacheService _cache;
 
         private readonly Logger _log = LogManager.GetLogger(nameof(SberService));
 
         public SberService(
             IConversationService conversationService,
             IMapper mapper,
-            IDialogflowService dialogflowService) : base(conversationService, mapper, dialogflowService)
+            IDialogflowService dialogflowService, IRedisCacheService cache) : base(conversationService, mapper, dialogflowService)
         {
             _mapper = mapper;
+            _cache = cache;
+        }
+
+        protected override Models.Request Before(Request input)
+        {
+            var request = base.Before(input);
+
+            var userStateCacheKey = GetCacheKey(request.UserHash);
+
+            _cache.TryGet(userStateCacheKey, out UserState userState);
+
+            request.IsOldUser = userState?.IsOldUser ?? false;
+
+            request.NextTextIndex = Convert.ToInt32(userState?.NextTextIndex ?? 0);
+
+            var contexts = GetContexts(input);
+
+            request.RequiredContexts.AddRange(contexts);
+
+            return request;
         }
 
         protected override async Task<Response> AfterAsync(Request input, Models.Response response)
@@ -28,7 +54,33 @@ namespace FillInTheTextBot.Messengers.Sber
 
             _mapper.Map(input, output);
 
+            var userState = new UserState
+            {
+                IsOldUser = true,
+                NextTextIndex = response.NextTextIndex
+            };
+
+            var userStateCacheKey = GetCacheKey(response.UserHash);
+
+            await _cache.TryAddAsync(userStateCacheKey, userState);
+
             return output;
+        }
+
+        private IEnumerable<Context> GetContexts(Request input)
+        {
+            var context = new Context
+            {
+                Name = input.Payload.Character.Id,
+                LifeSpan = 50000
+            };
+
+            return new[] { context };
+        }
+
+        private string GetCacheKey(string key)
+        {
+            return $"sber:{key}";
         }
     }
 }
---
+++ src/FillInTheTextBot.Messengers.Sber/UserState.cs
@@ -0,0 +1,9 @@
+﻿namespace FillInTheTextBot.Messengers.Sber
+{
+    public class UserState
+    {
+        public bool IsOldUser { get; set; }
+        
+        public int NextTextIndex { get; set; }
+    }
+}
