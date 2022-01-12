1cb283d94ee8a1c2e6eea12ce49d18be40583925
Stepan Grankin <sgrankin@inbox.ru>
2022-01-12 08:55:12 +0300

    try to create second test

--- src/FillInTheTextBot.Services.Tests/MappingProfiles/DialogflowMappingTests.cs
+++ src/FillInTheTextBot.Services.Tests/MappingProfiles/DialogflowMappingTests.cs
@@ -1,5 +1,6 @@
 ﻿using FillInTheTextBot.Services.Mapping;
 using Google.Cloud.Dialogflow.V2;
+using Google.Protobuf.WellKnownTypes;
 using NUnit.Framework;
 
 namespace FillInTheTextBot.Services.Tests.MappingProfiles
@@ -12,6 +13,21 @@ namespace FillInTheTextBot.Services.Tests.MappingProfiles
             var source = new QueryResult();
 
             var dialog = source.ToDialog();
+
+            Assert.IsEmpty(dialog.Parameters);
+        }
+
+        [Test]
+        public void ToDialog_Parameters_EmptyDictionary()
+        {
+            var source = new QueryResult();
+            source.Parameters = new Struct();
+            source.Parameters.Fields.Add<string, StringValue>("test", new StringValue());
+
+
+            var dialog = source.ToDialog();
+
+            Assert.IsEmpty(dialog.Parameters);
         }
     }
 }
