e6c9f11daa7417218a3f8e96f554caff3c09c38f
Stepan Grankin <s.grankin@dodopizza.com>
2022-01-13 09:31:39 +0300

    ToDialog_ParametersWithStringValue_ContainsKeyValuePair passed

--- src/FillInTheTextBot.Services.Tests/MappingProfiles/DialogflowMappingTests.cs
+++ src/FillInTheTextBot.Services.Tests/MappingProfiles/DialogflowMappingTests.cs
@@ -1,4 +1,5 @@
-﻿using FillInTheTextBot.Services.Mapping;
+﻿using AutoFixture;
+using FillInTheTextBot.Services.Mapping;
 using Google.Cloud.Dialogflow.V2;
 using Google.Protobuf.WellKnownTypes;
 using NUnit.Framework;
@@ -7,6 +8,14 @@ namespace FillInTheTextBot.Services.Tests.MappingProfiles
 {
     internal class DialogflowMappingTests
     {
+        private Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture { OmitAutoProperties = true };
+        }
+
         [Test]
         public void ToDialog_NullParameters_EmptyDictionary()
         {
@@ -18,16 +27,25 @@ namespace FillInTheTextBot.Services.Tests.MappingProfiles
         }
 
         [Test]
-        public void ToDialog_Parameters_EmptyDictionary()
+        public void ToDialog_ParametersWithStringValue_ContainsKeyValuePair()
         {
             var source = new QueryResult();
             source.Parameters = new Struct();
-            source.Parameters.Fields.Add<string, StringValue>("test", new StringValue());
+
+            var key = _fixture.Create<string>();
+            var value = _fixture.Create<string>();
+            
+            source.Parameters.Fields.Add(key, new Value
+            {
+                StringValue = value
+            });
 
 
             var dialog = source.ToDialog();
 
-            Assert.IsEmpty(dialog.Parameters);
+            Assert.IsNotEmpty(dialog.Parameters, "Parameters should not be empty");
+            Assert.True(dialog.Parameters.ContainsKey(key));
+            Assert.True(dialog.Parameters.Values.Contains(value));
         }
     }
 }
