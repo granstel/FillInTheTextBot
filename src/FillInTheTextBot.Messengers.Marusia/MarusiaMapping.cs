3513776dcd490252ea41b9953bcd3120c68821f9
Stepan Grankin <sgrankin@inbox.ru>
2022-03-02 15:26:57 +0300

    finish marusia mapping

--- src/FillInTheTextBot.Messengers.Marusia/MarusiaMapping.cs
+++ src/FillInTheTextBot.Messengers.Marusia/MarusiaMapping.cs
@@ -8,9 +8,6 @@ using MarusiaModels = MailRu.Marusia.Models;
 
 namespace FillInTheTextBot.Messengers.Marusia
 {
-    /// <summary>
-    /// Probably, registered at MappingRegistration of "Api" project
-    /// </summary>
     public static class MarusiaMapping
     {
         public static Models.Request ToRequest(this InputModel source)
@@ -32,6 +29,17 @@ namespace FillInTheTextBot.Messengers.Marusia
             destinaton.Appeal = Appeal.NoOfficial;
 
             return destinaton;
+        }
+
+        public static OutputModel FillOutput(this InputModel source, OutputModel destination)
+        {
+            if (source == null) return null;
+            if (destination == null) return null;
+
+            destination.Session = source.Session;
+            destination.Version = source.Version;
+
+            return destination;
         }
 
         public static OutputModel ToOutput(this Models.Response source)
@@ -60,6 +68,18 @@ namespace FillInTheTextBot.Messengers.Marusia
             return destination;
         }
 
+        public static Session ToSession(this Models.Response source)
+        {
+            if (source == null) return null;
+
+            var destination = new Session
+            {
+                UserId = source.UserHash
+            };
+
+            return destination;
+        }
+
         public static ResponseButton[] ToResponseButtons(this ICollection<Models.Button> source)
         {
             if (source == null) return null;
@@ -79,54 +99,5 @@ namespace FillInTheTextBot.Messengers.Marusia
 
             return responseButtons.ToArray();
         }
-
-        public static Session ToSession(this Models.Response source)
-        {
-            if (source == null) return null;
-
-            var destination = new Session
-            {
-                UserId = source.UserHash
-            };
-
-            return destination;
-        }
-
-        //public MarusiaMapping()
-        //{
-        //    CreateMap<Response, MarusiaModels.OutputModel>()
-        //        .ForMember(d => d.Response, m => m.MapFrom(s => s))
-        //        .ForMember(d => d.Session, m => m.MapFrom(s => s))
-        //        .ForMember(d => d.Version, m => m.Ignore())
-        //        .ForMember(d => d.UserStateUpdate, m => m.Ignore())
-        //        .ForMember(d => d.SessionState, m => m.Ignore());
-
-        //    CreateMap<Response, MarusiaModels.Response>()
-        //        .ForMember(d => d.Text, m => m.MapFrom(s => s.Text.Replace(Environment.NewLine, "\n")))
-        //        .ForMember(d => d.Tts, m => m.MapFrom(s => s.AlternativeText.Replace(Environment.NewLine, "\n")))
-        //        .ForMember(d => d.EndSession, m => m.MapFrom(s => s.Finished))
-        //        .ForMember(d => d.Buttons, m => m.MapFrom(s => s.Buttons))
-        //        .ForMember(d => d.Card, m => m.Ignore());
-
-        //    CreateMap<Response, MarusiaModels.Session>()
-        //        .ForMember(d => d.UserId, m => m.MapFrom(s => s.UserHash))
-        //        .ForMember(d => d.MessageId, m => m.Ignore())
-        //        .ForMember(d => d.SessionId, m => m.Ignore())
-        //        .ForMember(d => d.Application, m => m.Ignore())
-        //        .ForMember(d => d.User, m => m.Ignore());
-
-        //    CreateMap<InputModel, MarusiaModels.OutputModel>()
-        //        .ForMember(d => d.Session, m => m.MapFrom(s => s.Session))
-        //        .ForMember(d => d.Version, m => m.MapFrom(s => s.Version))
-        //        .ForMember(d => d.Response, m => m.Ignore())
-        //        .ForMember(d => d.UserStateUpdate, m => m.Ignore())
-        //        .ForMember(d => d.SessionState, m => m.Ignore());
-
-        //    CreateMap<Models.Button, ResponseButton>()
-        //        .ForMember(d => d.Title, m => m.MapFrom(s => s.Text))
-        //        .ForMember(d => d.Url, m => m.MapFrom(s => !string.IsNullOrEmpty(s.Url) ? s.Url : null))
-        //        .ForMember(d => d.Hide, m => m.MapFrom(s => s.IsQuickReply))
-        //        .ForMember(d => d.Payload, m => m.Ignore());
-        //}
     }
 }
