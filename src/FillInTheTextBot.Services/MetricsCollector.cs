f7edfec1cd4aaa56eb45cacf8131307a3dfa3759
Stepan Grankin <sgrankin@inbox.ru>
2023-02-14 21:34:28 +0300

    MetricsCollector

---
+++ src/FillInTheTextBot.Services/MetricsCollector.cs
@@ -0,0 +1,19 @@
+﻿using Prometheus;
+
+namespace FillInTheTextBot.Services;
+
+public static class MetricsCollector
+{
+    private static readonly Gauge _statistics;
+
+    static MetricsCollector()
+    {
+        _statistics = Metrics
+            .CreateGauge("statistics", "Statistics", "statistic_name", "parameter");
+    }
+
+    public static void Increment(string key, string value)
+    {
+        _statistics.WithLabels(key, value).Inc();
+    }
+}
