ce80e30cf0b81ab7c9081ef1f713a97e1efaafab
Stepan Grankin <s.grankin@dodobrands.io>
2025-07-02 23:02:13 +0300

    refactored

--- src/FillInTheTextBot.Services/MetricsCollector.cs
+++ src/FillInTheTextBot.Services/MetricsCollector.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 
 namespace FillInTheTextBot.Services;
@@ -6,13 +6,12 @@ namespace FillInTheTextBot.Services;
 public static class MetricsCollector
 {
     public const string MeterName = "FillInTheTextBot.Metrics";
-    private static readonly Meter Meter;
     private static readonly Counter<long> MetricsCounter;
 
     static MetricsCollector()
     {
-        Meter = new Meter(MeterName, "1.0.0");
-        MetricsCounter = Meter.CreateCounter<long>("metrics", "Custom metrics");
+        var meter = new Meter(MeterName, "1.0.0");
+        MetricsCounter = meter.CreateCounter<long>("metrics", "Custom metrics");
     }
 
     public static void Increment(string key, string value)
