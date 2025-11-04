671f290e4acc6814bcda9eb2bdaf5dc4d30427d5
Stepan Grankin <s.grankin@dodobrands.io>
2025-11-04 19:34:08 +0300

    DialogflowEmulatorService

--- src/Dialogflow.Emulator/Program.cs
+++ src/Dialogflow.Emulator/Program.cs
@@ -16,6 +16,7 @@ await agentStorage.InitializeAsync(agentPath);
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DialogflowEmulatorService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
